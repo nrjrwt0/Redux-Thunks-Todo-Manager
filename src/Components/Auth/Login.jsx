4655e7ef2ea6a578755df229f69f6314ad71fa38
Neeraj Rawat <neerajrawatnt@gmail.com>
2021-04-30 22:44:49 +0530

    local-storage and onChange event on form input added

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -7,13 +7,14 @@ const Login = () => {
   const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
 
+
   return(
     <div>
       <Container>
         <h1>Login</h1>
           <form>
-            <input type="text" placeholder="Email"/>
-            <input type="text" placeholder="Password"/>
+            <input required type="email" placeholder="Email" onChange={(e) => {setEmail(e.target.value(e))}}/>
+            <input required type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value(e))}}/>
             <input type="submit" value="Login"/>
           </form>
          <span>Don't have an account <Link to="/signup">Sign up</Link> here</span>
--- src/Components/Auth/SignUp.jsx
+++ src/Components/Auth/SignUp.jsx
@@ -2,19 +2,34 @@ import React from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
 
+const initState = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+}
+
 const SignUp = () => {
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
+  const [formData,setFormData] = useState(initState);
+
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    const payload = {
+      ...formData,
+      [name]: value
+    }
+    setFormData(payload)
+  }
 
   return(
     <div>
       <div>
       <h1>Sign Up</h1>
         <form>
-        <input type="text" placeholder="Name"/>
-        <input type="text" placeholder="Email"/>
-          <input type="text" placeholder="Password"/>
-          <input type="text" placeholder="Confirm Password"/>
+        <input type="text" placeholder="Name" name="name" onChange={handleChange}/>
+        <input type="text" placeholder="Email" name="email" onChange={handleChange}/>
+          <input type="text" placeholder="Password" name="password" onChange={handleChange}/>
+          <input type="text" placeholder="Confirm Password" name="confirmPassword" onChange={handleChange}/>
           <input type="submit" value="Signup"/>
         </form>
         <span>Already have an account <Link to="/login">Login</Link> here</span>
